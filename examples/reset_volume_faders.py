a8ea2b574c1e734f057473538edadfb68b3b43c9
Eugene Cherny <login@eugn.ch>
2021-06-23 04:23:12 +0800

    add @wh_ignore tag for reset_volume_faders example

--- examples/reset_volume_faders.py
+++ examples/reset_volume_faders.py
@@ -3,8 +3,12 @@ from waapi import WaapiClient
 from waapi_helpers import *
 
 with WaapiClient() as client:
-    for bus_guid, in walk_wproj(client, '\\Master-Mixer Hierarchy', types=['Bus', 'AuxBus']):
-        set_property_value(client, bus_guid, 'BusVolume', 0)
+    for bus_guid, bus_notes in walk_wproj(client, '\\Master-Mixer Hierarchy',
+                                          properties=['id', 'notes'], types=['Bus', 'AuxBus']):
+        if '@wh_ignore' not in bus_notes:
+            set_property_value(client, bus_guid, 'BusVolume', 0)
 
-    for am_guid, in walk_wproj(client, '\\Actor-Mixer Hierarchy', types=['ActorMixer']):
-        set_property_value(client, am_guid, 'Volume', 0)
+    for am_guid, am_notes, in walk_wproj(client, '\\Actor-Mixer Hierarchy',
+                                         properties=['id', 'notes'], types=['ActorMixer']):
+        if '@wh_ignore' not in am_notes:
+            set_property_value(client, am_guid, 'Volume', 0)
